cfe66300eaf1686114d4e6f10978dcd656f80816
Nikolay Lebedinskiy <nikolaylebedinskiy1983.ru>
2022-09-15 14:37:00 +0300

    Задача 25

---
+++ object25/Program.cs
@@ -0,0 +1,17 @@
+﻿System.Console.Write("Введите число A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+
+System.Console.Write("Введиту число B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+
+int Exponent(int A, int B) {
+    int result = 1;
+    for(int i = 1; i<= B; i++) {
+        result = result * A;
+    }
+    return result;
+}
+int exponent = Exponent( A, B);
+System.Console.WriteLine($"Число А в степени В равно: " + exponent);
+
+
