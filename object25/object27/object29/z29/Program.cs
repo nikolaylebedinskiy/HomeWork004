b81c1f478d7e3cbb615d40fb69543badac83992a
Nikolay Lebedinskiy <nikolaylebedinskiy1983.ru>
2022-09-15 17:23:49 +0300

    Задача 29

---
+++ object25/object27/object29/z29/Program.cs
@@ -0,0 +1,15 @@
+﻿int [] GetArray(){
+    int[] result = new int[8];
+    for (int i = 0; i < result.Length; i++) {
+        result[i] = new Random().Next(100);
+    }
+    return result;
+}
+
+void PrintArray(int[] arr) {
+    for (int i = 0; i < arr.Length; i++) {
+        System.Console.WriteLine(arr[i]);
+    }
+}
+
+PrintArray(GetArray());/
